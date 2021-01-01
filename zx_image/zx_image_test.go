a1907e360d39df596239167414249ff703de11a6
wallermatt <waller.matthew.alt@gmail.com>
2021-01-01 16:59:39 +0000

    tests

--- zx_image/zx_image_test.go
+++ zx_image/zx_image_test.go
@@ -10,4 +10,14 @@ import (
 func Test_ReadSnapshot(t *testing.T) {
 	s := zx_image.ReadSnapshot("testData/testSnapshot.sna")
 	assert.Equal(t, 49179, len(s))
+	assert.Equal(t, uint8(0x3f), s[0])
+	assert.Equal(t, uint8(0x0), s[len(s)-1])
+}
+
+func Test_LoadScrMemory(t *testing.T) {
+	s := zx_image.ReadSnapshot("testData/testSnapshot.sna")
+	scrMemory := zx_image.LoadScrMemory(s)
+	assert.Equal(t, uint8(0x0), scrMemory[0][0])
+	assert.Equal(t, uint8(0xff), scrMemory[110][25])
+	assert.Equal(t, uint8(0x0), scrMemory[191][31])
 }
